0331cf3b659b0c3b828bdc28ad242fc831f12488
Thridev Maharajh <maharajhthridev@gmail.com>
2025-07-02 19:55:35 +0200

    Minor Fix to Buttons

--- SmartStudyCompanion/Forms/TimerScreen.Designer.cs
+++ SmartStudyCompanion/Forms/TimerScreen.Designer.cs
@@ -49,6 +49,7 @@ namespace SmartStudyCompanion.Forms
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(34, 101);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 37);
@@ -61,6 +62,7 @@ namespace SmartStudyCompanion.Forms
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(163, 101);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(87, 37);
@@ -73,6 +75,7 @@ namespace SmartStudyCompanion.Forms
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(277, 101);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(87, 37);
