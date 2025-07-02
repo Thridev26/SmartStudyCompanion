3a7328a743b089e043b0ebb9d64a5207a3ec35b8
Thridev Maharajh <maharajhthridev@gmail.com>
2025-07-02 18:23:52 +0200

    Buttons added to dahsboard

--- SmartStudyCompanion/Forms/MainDashboard.Designer.cs
+++ SmartStudyCompanion/Forms/MainDashboard.Designer.cs
@@ -32,11 +32,20 @@ namespace SmartStudyCompanion
             this.TopPanel = new System.Windows.Forms.Panel();
             this.SidePanel = new System.Windows.Forms.Panel();
             this.MainContentPanel = new System.Windows.Forms.Panel();
+            this.TopLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.TopPanel.SuspendLayout();
+            this.SidePanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // TopPanel
             // 
             this.TopPanel.BackColor = System.Drawing.Color.SteelBlue;
+            this.TopPanel.Controls.Add(this.TopLabel);
             this.TopPanel.Location = new System.Drawing.Point(0, 0);
             this.TopPanel.Name = "TopPanel";
             this.TopPanel.Size = new System.Drawing.Size(1000, 80);
@@ -45,6 +54,11 @@ namespace SmartStudyCompanion
             // SidePanel
             // 
             this.SidePanel.BackColor = System.Drawing.Color.LightGray;
+            this.SidePanel.Controls.Add(this.button5);
+            this.SidePanel.Controls.Add(this.button4);
+            this.SidePanel.Controls.Add(this.button3);
+            this.SidePanel.Controls.Add(this.button2);
+            this.SidePanel.Controls.Add(this.button1);
             this.SidePanel.Location = new System.Drawing.Point(0, 86);
             this.SidePanel.Name = "SidePanel";
             this.SidePanel.Size = new System.Drawing.Size(200, 616);
@@ -58,6 +72,68 @@ namespace SmartStudyCompanion
             this.MainContentPanel.Size = new System.Drawing.Size(794, 613);
             this.MainContentPanel.TabIndex = 2;
             // 
+            // TopLabel
+            // 
+            this.TopLabel.AutoSize = true;
+            this.TopLabel.BackColor = System.Drawing.Color.Transparent;
+            this.TopLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TopLabel.Location = new System.Drawing.Point(312, 9);
+            this.TopLabel.Name = "TopLabel";
+            this.TopLabel.Size = new System.Drawing.Size(431, 48);
+            this.TopLabel.TabIndex = 0;
+            this.TopLabel.Text = "Smart Study Companion";
+            this.TopLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dashboard";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start Timer";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Tasks";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(12, 211);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 45);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Reports";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(12, 276);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(180, 45);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Settings";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // MainDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -69,6 +145,9 @@ namespace SmartStudyCompanion
             this.Name = "MainDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            this.SidePanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -78,6 +157,12 @@ namespace SmartStudyCompanion
         private System.Windows.Forms.Panel TopPanel;
         private System.Windows.Forms.Panel SidePanel;
         private System.Windows.Forms.Panel MainContentPanel;
+        private System.Windows.Forms.Label TopLabel;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
