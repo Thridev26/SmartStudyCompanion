e1a2a6568f08acea3d80f6c7f0a8174049672685
Thridev Maharajh <maharajhthridev@gmail.com>
2025-07-02 19:54:41 +0200

    Buttons Added to Timer Screen

--- SmartStudyCompanion/Forms/TimerScreen.Designer.cs
+++ SmartStudyCompanion/Forms/TimerScreen.Designer.cs
@@ -30,6 +30,9 @@ namespace SmartStudyCompanion.Forms
         private void InitializeComponent()
         {
             this.TimerLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // TimerLabel
@@ -41,11 +44,51 @@ namespace SmartStudyCompanion.Forms
             this.TimerLabel.TabIndex = 0;
             this.TimerLabel.Text = "00:25:00";
             // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(34, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(163, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(277, 101);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // TimerScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.TimerLabel);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Name = "TimerScreen";
             this.Size = new System.Drawing.Size(400, 250);
             this.ResumeLayout(false);
@@ -56,5 +99,8 @@ namespace SmartStudyCompanion.Forms
         #endregion
 
         private System.Windows.Forms.Label TimerLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
