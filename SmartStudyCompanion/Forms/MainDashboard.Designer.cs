9147883508987c2bac646cb82ca845857fcb16c4
Thridev Maharajh <maharajhthridev@gmail.com>
2025-07-02 18:14:08 +0200

    UI Panels added

---
+++ SmartStudyCompanion/Forms/MainDashboard.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace SmartStudyCompanion
+{
+    partial class MainDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.SidePanel = new System.Windows.Forms.Panel();
+            this.MainContentPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.SteelBlue;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1000, 80);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // SidePanel
+            // 
+            this.SidePanel.BackColor = System.Drawing.Color.LightGray;
+            this.SidePanel.Location = new System.Drawing.Point(0, 86);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(200, 616);
+            this.SidePanel.TabIndex = 1;
+            // 
+            // MainContentPanel
+            // 
+            this.MainContentPanel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.MainContentPanel.Location = new System.Drawing.Point(206, 86);
+            this.MainContentPanel.Name = "MainContentPanel";
+            this.MainContentPanel.Size = new System.Drawing.Size(794, 613);
+            this.MainContentPanel.TabIndex = 2;
+            // 
+            // MainDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 712);
+            this.Controls.Add(this.MainContentPanel);
+            this.Controls.Add(this.SidePanel);
+            this.Controls.Add(this.TopPanel);
+            this.Name = "MainDashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Panel SidePanel;
+        private System.Windows.Forms.Panel MainContentPanel;
+    }
+}
+
